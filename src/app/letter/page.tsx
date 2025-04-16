d7e2159ce72570159069d15eff01cd906101b239
Adeeb <amashriq@terpmail.umd.edu>
2025-04-16 01:11:49 -0400

    vercel

--- src/app/letter/page.tsx
+++ src/app/letter/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function letter() {
   return <div></div>;
 }
--- src/app/memories/page.tsx
+++ src/app/memories/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function memories() {
   return <div></div>;
 }
