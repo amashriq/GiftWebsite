7aa5bb795a6723f03205f635368e2a586fd56738
Adeeb <amashriq@terpmail.umd.edu>
2025-04-25 15:27:49 -0400

    game buttons and aesthetics

--- src/app/game/GameClient.tsx
+++ src/app/game/GameClient.tsx
@@ -2,6 +2,8 @@
 
 import React, { useState, useEffect, useCallback } from "react";
 import Image from "next/image";
+import ActionButton from "@/components/ActionButton";
+import CheckIcon from "@/components/icons/CheckIcon";
 
 export interface GameImage {
   src: string;
@@ -38,6 +40,7 @@ export default function GameClient({ goodImages, badImages }: GameClientProps) {
     "playing"
   );
   const [message, setMessage] = useState<string>("Pick all the good pictures!");
+  const [isFadingOut, setIsFadingOut] = useState(false);
 
   const TOTAL_IMAGES_PER_ROUND = 10; //subject to change
   const IMAGES_PER_TYPE = TOTAL_IMAGES_PER_ROUND / 2;
@@ -92,76 +95,94 @@ export default function GameClient({ goodImages, badImages }: GameClientProps) {
   };
 
   const handleCheckAnswer = () => {
+    if (isFadingOut) return;
     if (gameState !== "playing") return;
+
+    setIsFadingOut(true);
+    const transitionDuration = 300;
+
     const winConditionMet = selectedImages.size === TOTAL_IMAGES_PER_ROUND;
 
-    if (winConditionMet) {
-      setGameState("won");
-      setMessage("Congratulations! You picked all the good pictures!");
-    } else {
-      setGameState("lost");
-      setMessage("maybe next time! You picked some bad pictures."); //diff messages based on how many she chose
-    }
+    setTimeout(() => {
+      if (winConditionMet) {
+        setGameState("won");
+        setMessage("Congratulations! You picked all the good pictures!");
+      } else {
+        setGameState("lost");
+        setMessage("maybe next time! You picked some bad pictures."); //diff messages based on how many she chose
+      }
+    }, transitionDuration);
   };
 
-  return (
-    <div className="flex w-full flex-col items-center p-4">
-      <h1 className="text-3x1 font-bold mb-4"> Game Time!</h1>
-      <p className="mb-6 text-lg text-center">{message}</p>
+  useEffect(() => {
+    if (gameState === "playing") {
+      setIsFadingOut(false);
+    }
+  }, [gameState]);
 
-      <div className="flex flex-wrap justify-center items-center gap-4 mb-8 w-full">
-        {currentImages.map((img) => (
+  return (
+    <div className="flex h-[95vh] w-[vw] flex-col justify-center items-center p-6 mt-10">
+      {gameState === "playing" ? (
+        <>
           <div
-            key={img.src}
-            onClick={() => handleImageSelect(img.src)}
-            className={`flex-1 min-w-[140px] relative border-4 rounded-lg transition-all duration-200 ease-in-out group${
-              selectedImages.has(img.src)
-                ? "boarder-green-500 shadow-lg"
-                : "border-transparent hover:border-gray-400"
-            } ${
-              gameState !== "playing"
-                ? "opacity-70 cursor-not-allowed pointer-events-none"
-                : "cursor-pointer"
+            className={`flex flex-wrap justify-center items-center gap-10 mb-8 transition-opacity duration-300 ease-in-out ${
+              isFadingOut ? "opacity-0" : "opacity-100"
             }`}
           >
-            <Image
-              src={img.src}
-              alt={img.filename}
-              width={img.width}
-              height={img.height}
-              className="block w-full h-auto align-middle rounded-md group-hover:opacity-85 transition-opacity"
-              priority
-            />
+            {currentImages.map((img) => (
+              <div
+                key={img.src}
+                onClick={() => handleImageSelect(img.src)}
+                className={`flex h-[17.5vw] rounded-lg ring-4 ring-[#A26769] cursor-pointer transition duration-300 ease-in-out transform hover:shadow-lg hover:scale-105 hover:ring-6 ${
+                  selectedImages.has(img.src)
+                    ? "shadow-lg !ring-[8px] scale-105 !ring-[#F4C2C2]"
+                    : ""
+                }`}
+              >
+                <Image
+                  src={img.src}
+                  alt={img.filename}
+                  width={img.width}
+                  height={img.height}
+                  className={`block w-full h-full rounded-lg transition duration-300 ease-in-out transform hover:brightness-80`}
+                  priority
+                />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="flex gap-4 h-10 items-center">
-        {gameState === "playing" &&
-          currentImages.length === TOTAL_IMAGES_PER_ROUND && (
+          <div className="flex gap-4 items-center">
+            {currentImages.length === TOTAL_IMAGES_PER_ROUND && (
+              <ActionButton
+                onClick={handleCheckAnswer}
+                icon={<CheckIcon className="w-20 h-12" stroke="#D8A39D" />}
+                aria-label="Check Answer"
+                className={`${
+                  isFadingOut ? "opacity-0 pointer-events-none" : "opacity-100"
+                }`}
+              />
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          {gameState === "lost" && (
             <button
-              onClick={handleCheckAnswer}
-              className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition shadow"
+              onClick={setupNewGame}
+              className="px-6 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition shadow"
             >
-              Check Answer
+              Try Again
             </button>
           )}
-        {gameState === "lost" && (
-          <button
-            onClick={setupNewGame}
-            className="px-6 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition shadow"
-          >
-            Try Again
-          </button>
-        )}
-        {gameState === "won" && (
-          <button
-            onClick={setupNewGame}
-            className="px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition shadow"
-          >
-            Play Again?
-          </button>
-        )}
-      </div>
+          {gameState === "won" && (
+            <button
+              onClick={setupNewGame}
+              className="px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition shadow"
+            >
+              Play Again?
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Link from "next/link";
+
+interface ActionButtonProps {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  icon: React.ReactNode;
+  "aria-label": string;
+  className?: string;
+}
+
+const ActionButton = ({
+  onClick,
+  icon,
+  className,
+  "aria-label": ariaLabel,
+}: ActionButtonProps) => {
+  const buttonBaseClasses = `
+        group 
+        flex 
+        items-center 
+        justify-center 
+        p-4 
+        h-15 w-30
+        bg-[#A8BBA8] 
+        rounded-xl 
+        border-2 border-[#97A897] 
+        shadow-md 
+        cursor-pointer 
+        duration-300 ease-in-out 
+        transform
+        transition
+        hover:brightness-80 hover:shadow-lg hover:scale-105
+    `;
+
+  const combinedClasses = `${buttonBaseClasses} ${className || ""}`;
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={combinedClasses.trim()}
+      aria-label={ariaLabel}
+    >
+      <div
+        className={`
+            text-[#FFE5B4]
+            `}
+      >
+        {icon}
+      </div>
+    </button>
+  );
+};
+
+export default ActionButton;
